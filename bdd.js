de10b635166a0c3a4c822f1642ee5f2b7e60bc9d
Rayane Ben Ahmed <rayane.benahmed@outlook.fr>
2022-04-05 16:13:26 +0200

    ajout de getUser

--- bdd.js
+++ bdd.js
@@ -3,18 +3,46 @@ const uri = "mongodb+srv://Ray:test@todolist.lgwkb.mongodb.net/myFirstDatabase?r
 const client = new MongoClient(uri);
 
 
+function getUser(username,password){  
+  let query = { name : username, password : password};
+
+  client.connect((err)=>{
+    if (err) {
+      console.log("Erreur lors de la connection à la base de données");
+    }
+    else{
+      const collection = client.db("TODOList").collection("users");
+      collection.find(query).toArray((err, result) =>{
+        if(result.length == 0)
+          console.log("Utilisateur introuvable");
+          
+        else
+          console.log(result);
+          
+          client.close();
+      });
+    }
+  });
+}
+
+getUser("loic","kebab");
+
+
+
+/*
 client.connect((err) => {
   if (err) {
     console.log("Erreur lors de la connection à la base de données");
   } else {
     const collection = client.db("TODOList").collection("users");
 
-    let test = collection.updateOne({
+   collection.findOne({
       name : "loic"
-    },{$set: {password: "rayane"}},  (err) => {
+    },(err) => {
       if (err) {
         // do something
       } else {
         client.close();
       }
-    });}});
+    });}
+  });*/
